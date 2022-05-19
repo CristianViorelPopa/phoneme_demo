57b2117bc8bbf316764b438eebeade756b712c11
CristianViorelPopa <cristian.viorel.popa@gmail.com>
2022-05-19 16:14:11 +0300

    Updates

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,7 @@
 import streamlit as st
+
+from Levenshtein import distance as lev
+
 from metaphone import doublemetaphone
 from g2p_en import G2p
 g2p = G2p()
@@ -19,15 +22,36 @@ transcribe_button = st.button('Transcribe!')
 
 if transcribe_button:
     if algo == 'Grapheme to Phoneme':
+        first_phonetics = g2p(first_input)
+        second_phonetics = g2p(second_input)
+
+        st.write(f'Phonetic transcription of first text: {first_phonetics}')
+        st.write(f'Phonetic transcription of second text: {second_phonetics}')
+
+    elif algo == 'Double Metaphone':
         first_phonetics = list(filter(len, doublemetaphone(first_input)))
         second_phonetics = list(filter(len, doublemetaphone(second_input)))
 
-        first_phonetics_message = 'Phonetic transcription(s) of first text:\n'
+        st.write('Phonetic transcription(s) of the first text:')
         for idx, phonetic in enumerate(first_phonetics):
-            first_phonetics_message += f'\t{idx + 1}. {phonetic}\n'
-        st.write(first_phonetics_message)
+            st.write(f'{idx + 1}. {phonetic}')
 
-        second_phonetics_message = 'Phonetic transcription(s) of first text:\n'
+        st.write('Phonetic transcription(s) of the second text:')
         for idx, phonetic in enumerate(second_phonetics):
-            second_phonetics_message += f'\t{idx + 1}. {phonetic}\n'
-        st.write(second_phonetics_message)
+            st.write(f'{idx + 1}. {phonetic}')
+
+        min_error = 999_999
+        chosen_first_phonetic = first_phonetics[0]
+        chosen_second_phonetic = second_phonetics[0]
+        for phonetic1 in first_phonetics:
+            for phonetic2 in second_phonetics:
+                dist = lev(phonetic1, phonetic2)
+                if dist < min_error:
+                    min_error = dist
+                    chosen_first_phonetic = phonetic1
+                    chosen_second_phonetic = phonetic2
+
+        st.write(f'Chosen phonetic transcriptions for a minimum Levenshtein distance of {min_error}: {chosen_first_phonetic} and {chosen_second_phonetic}')
+
+    else:
+        raise RuntimeError('Algorithm not yet implemented.')
